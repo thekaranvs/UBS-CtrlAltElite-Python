363d0c4f0341040a50367b71ada815cc2a774d82
Firedrake2002 <sumerkaistha@gmail.com>
2023-09-23 12:23:33 +0800

    changed greedymonkey to dp solution

--- solutions/greedyMonkey.py
+++ solutions/greedyMonkey.py
@@ -10,43 +10,61 @@ def returnFruits(text):
     v = case["v"]
     f = case["f"]
 
-    currWeight = 0
-    currVolume = 0
-    values = []
-    for i in range(len(f)):
-        if (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) <= v:
-            values.append((f[i][2], i))
-            currWeight += f[i][0]
-            currVolume += f[i][1]
-        elif (currWeight + f[i][0]) > w and (currVolume + f[i][1]) <= v:
-            values.sort()
-            for value in values:
-                if value[0] < f[i][2] and currWeight - f[value[1]][0] + f[i][0] <= w:
-                    values.remove(value)
-                    values.append((f[i][2], i))
-                    break
-        elif (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) > v:
-            values.sort()
-            for value in values:
-                if value[0] < f[i][2] and currVolume - f[value[1]][1] + f[i][1] <= v:
-                    values.remove(value)
-                    values.append((f[i][2], i))
-                    break
-        else:
-            values.sort()
-            for value in values:
-                if value[0] < f[i][2] and  currVolume - f[value[1]][1] + f[i][1] <= v and currWeight - f[value[1]][0] + f[i][0] <= w:
-                    values.remove(value)
-                    values.append((f[i][2], i))
-                    break
+    n = len(f)
+    dp = [[[0, ""] for _ in range(v + 1)] for _ in range(w + 1)]
+
+    for i in range(1, n + 1):
+        weight, volume, value = f[i-1]
+
+        for j in range(w+1):
+            for k in range(v+1):
+                if weight <= j and volume <= k:
+                    if dp[j - weight][k - volume][0] + value > dp[j][k][0]:
+                        dp[j][k][0] = dp[j - weight][k - volume][0] + value
+                        dp[j][k][1] = (dp[j - weight][k - volume][1] + str(i))
+
+
+    output = dp[w][v][1] + "0"
+
+    return output
+
+    # currWeight = 0
+    # currVolume = 0
+    # values = []
+    # for i in range(len(f)):
+    #     if (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) <= v:
+    #         values.append((f[i][2], i))
+    #         currWeight += f[i][0]
+    #         currVolume += f[i][1]
+    #     elif (currWeight + f[i][0]) > w and (currVolume + f[i][1]) <= v:
+    #         values.sort()
+    #         for value in values:
+    #             if value[0] < f[i][2] and currWeight - f[value[1]][0] + f[i][0] <= w:
+    #                 values.remove(value)
+    #                 values.append((f[i][2], i))
+    #                 break
+    #     elif (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) > v:
+    #         values.sort()
+    #         for value in values:
+    #             if value[0] < f[i][2] and currVolume - f[value[1]][1] + f[i][1] <= v:
+    #                 values.remove(value)
+    #                 values.append((f[i][2], i))
+    #                 break
+    #     else:
+    #         values.sort()
+    #         for value in values:
+    #             if value[0] < f[i][2] and  currVolume - f[value[1]][1] + f[i][1] <= v and currWeight - f[value[1]][0] + f[i][0] <= w:
+    #                 values.remove(value)
+    #                 values.append((f[i][2], i))
+    #                 break
         
-    output = ""
-    for value in values:
-        output += str(value[1]+1)
+    # output = ""
+    # for value in values:
+    #     output += str(value[1]+1)
 
-    output += "0"
+    # output += "0"
 
-    return(output)
+    # return(output)
 
 
 
