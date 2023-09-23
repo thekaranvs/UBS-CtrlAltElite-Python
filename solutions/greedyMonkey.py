4b3ccfd2a5bd992758343daa65530ea7f2e6a28a
Firedrake2002 <sumerkaistha@gmail.com>
2023-09-23 13:01:28 +0800

    switched to greedy solution for greedymonkeys

--- solutions/greedyMonkey.py
+++ solutions/greedyMonkey.py
@@ -10,60 +10,58 @@ def returnFruits(text):
     v = case["v"]
     f = case["f"]
 
-    n = len(f)
-    dp = [[0 for _ in range(v + 1)] for _ in range(w + 1)]
+    # n = len(f)
+    # dp = [[0 for _ in range(v + 1)] for _ in range(w + 1)]
 
-    for i in range(1, n + 1):
-        weight, volume, value = f[i-1]
+    # for i in range(1, n + 1):
+    #     weight, volume, value = f[i-1]
 
-        for j in range(w, weight-1, -1):
-            for k in range(v, volume-1, -1):
-                if weight <= j and volume <= k:
-                    if dp[j - weight][k - volume] + value > dp[j][k]:
-                        dp[j][k] = dp[j - weight][k - volume] + value
+    #     for j in range(w, weight-1, -1):
+    #         for k in range(v, volume-1, -1):
+    #             if weight <= j and volume <= k:
+    #                 if dp[j - weight][k - volume] + value > dp[j][k]:
+    #                     dp[j][k] = dp[j - weight][k - volume] + value
 
 
-    output = dp[w][v]
+    # output = dp[w][v]
 
-    return str(output)
+    # return str(output)
 
-    # currWeight = 0
-    # currVolume = 0
-    # values = []
-    # for i in range(len(f)):
-    #     if (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) <= v:
-    #         values.append((f[i][2], i))
-    #         currWeight += f[i][0]
-    #         currVolume += f[i][1]
-    #     elif (currWeight + f[i][0]) > w and (currVolume + f[i][1]) <= v:
-    #         values.sort()
-    #         for value in values:
-    #             if value[0] < f[i][2] and currWeight - f[value[1]][0] + f[i][0] <= w:
-    #                 values.remove(value)
-    #                 values.append((f[i][2], i))
-    #                 break
-    #     elif (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) > v:
-    #         values.sort()
-    #         for value in values:
-    #             if value[0] < f[i][2] and currVolume - f[value[1]][1] + f[i][1] <= v:
-    #                 values.remove(value)
-    #                 values.append((f[i][2], i))
-    #                 break
-    #     else:
-    #         values.sort()
-    #         for value in values:
-    #             if value[0] < f[i][2] and  currVolume - f[value[1]][1] + f[i][1] <= v and currWeight - f[value[1]][0] + f[i][0] <= w:
-    #                 values.remove(value)
-    #                 values.append((f[i][2], i))
-    #                 break
+    currWeight = 0
+    currVolume = 0
+    values = []
+    for i in range(len(f)):
+        if (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) <= v:
+            values.append((f[i][2], i))
+            currWeight += f[i][0]
+            currVolume += f[i][1]
+        elif (currWeight + f[i][0]) > w and (currVolume + f[i][1]) <= v:
+            values.sort()
+            for value in values:
+                if value[0] < f[i][2] and currWeight - f[value[1]][0] + f[i][0] <= w:
+                    values.remove(value)
+                    values.append((f[i][2], i))
+                    break
+        elif (currWeight + f[i][0]) <= w and (currVolume + f[i][1]) > v:
+            values.sort()
+            for value in values:
+                if value[0] < f[i][2] and currVolume - f[value[1]][1] + f[i][1] <= v:
+                    values.remove(value)
+                    values.append((f[i][2], i))
+                    break
+        else:
+            values.sort()
+            for value in values:
+                if value[0] < f[i][2] and  currVolume - f[value[1]][1] + f[i][1] <= v and currWeight - f[value[1]][0] + f[i][0] <= w:
+                    values.remove(value)
+                    values.append((f[i][2], i))
+                    break
         
-    # output = ""
-    # for value in values:
-    #     output += str(value[1]+1)
+    output = 0
+    for value in values:
+        output += value[0]
 
-    # output += "0"
-
-    # return(output)
+    return(str(output))
 
 
 
